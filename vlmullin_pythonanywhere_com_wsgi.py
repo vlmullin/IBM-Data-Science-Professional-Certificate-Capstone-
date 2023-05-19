8d6fd78dcf8464f6b9a2e7b2d1803eb77e181c70
Virginia L Mullins <105428814+vlmullin@users.noreply.github.com>
2023-05-19 12:08:55 +0300

    Update and rename adgsenpai_pythonanywhere_com_wsgi.py to vlmullin_pythonanywhere_com_wsgi.py

--- vlmullin_pythonanywhere_com_wsgi.py
+++ vlmullin_pythonanywhere_com_wsgi.py
@@ -8,7 +8,7 @@
 import sys
 
 # add your project directory to the sys.path
-project_home = '/home/adgsenpai'
+project_home = '/home/vlmullin'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
